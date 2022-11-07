49e441da4300ad7fde103854bc0cf54aa3db9a3b
lhpl <lhpl@LHPL.com>
2022-11-07 22:22:42 +0700

    TDD - step 1 : write fail test

---
+++ PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace PrimeService.Tests
+{
+    public class PrimeService_IsPrimeShould
+    {
+        [Fact]
+        public void IsPrime_InputIs1_ReturnFalse()
+        { 
+            var primeService = new PrimeService();
+            bool result = primeService.IsPrime(1);
+
+            Assert.False(result, "1 should not be prime");
+        }
+    }
+}
